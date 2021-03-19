88df552c1f3bfc0759942636614c56efe0ab89d3
fedengarcia <fedengarcia11@hotmail.com>
2021-03-19 18:03:04 -0300

    Incorporando scrollReveal.js

---
+++ js/revealAnimation.js
@@ -0,0 +1,21 @@
+const sr = ScrollReveal();
+
+sr.reveal('.noticias', {
+    duration: 2000,
+    // origin: 'top',
+    reset: true,
+    easing: 'ease-in-out'
+});
+
+
+sr.reveal('.revealFeatures', {
+    duration: 2000,
+    // origin: 'top',
+    reset: true,
+    easing: 'ease-in-out',
+
+});
+
+
+
+
