cddd2c4fbe5000d5f22dc6b71835491bdaf8bff2
fedengarcia <fedengarcia11@hotmail.com>
2021-03-22 16:13:45 -0300

    Modificacion en php

--- enviar.php
+++ enviar.php
@@ -5,13 +5,22 @@ $email = $_POST['email'];
 $asunto = $_POST['asunto'];
 $consulta = $_POST['consulta'];
 
-$destinatario_php = "fedengarcia11@hotmail.com";
-$asunto_php = "Contacto desde mi pagina web";
 
-$carta = "De: $nombre \n";
-$carta.= "Email: $email \n";
-$asunto.= "Asunto: $asunto \n";
-$consulta.= "Consulta: $consulta";
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-mail($destinatario_php,$asunto_php,$carta);
-header('Location: index.html');
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
+$mensaje .= "Asunto: " . $asunto . " \r\n";
+$mensaje .= "Mensaje: " . $consulta . "\r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'federico.garcia@etermax.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
