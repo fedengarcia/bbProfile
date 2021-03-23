1ed51f2f319ccea750551177020183cd8826e9c8
fedengarcia <fedengarcia11@hotmail.com>
2021-03-23 09:39:39 -0300

    Correcion php

--- enviar.php
+++ enviar.php
@@ -7,15 +7,15 @@ $consulta = $_POST['consulta'];
 
 
 $header = 'From: ' . $email . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$header.= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header.= "Mime-Version: 1.0 \r\n";
+$header.= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $email . " \r\n";
-$mensaje .= "Asunto: " . $asunto . " \r\n";
-$mensaje .= "Mensaje: " . $consulta . "\r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
+$mensaje.= "Su e-mail es: " . $email . " \r\n";
+$mensaje.= "Asunto: " . $asunto . " \r\n";
+$mensaje.= "Mensaje: " . $consulta . "\r\n";
+$mensaje.= "Enviado el " . date('d/m/Y', time());
 
 $para = 'federico.garcia@etermax.com';
 $asunto = 'Mensaje de mi sitio web';
